d00e10ccbc91d385a8a1161741da5cdc7e196b6b
peter <peter_cook@comcast.com>
2022-08-03 18:05:23 -0400

    unfinished exercise 3

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,38 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+    }
+  }
+  handleLand() {
+    let result = window.confirm('Are you sure the shuttle is ready to land?');
+    if (result) {
+      this.color = 'green';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+    }
+  }
+  handleAbort() {
+    let result = window.confirm('Are you want to abort?');
+    if (result) {
+      this.color = 'red';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+    }
+  }
+moveRocket(rocketImage, direction){
+  let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+  rocketImage.style.left = movement;
+  this.width= this.width+10000;
+}
+
 }
